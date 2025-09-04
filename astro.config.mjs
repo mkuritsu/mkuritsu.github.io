a58c2f8d8efac92a4bd78278b4cbf3b6127859e6
mkuritsu <mkuritsuu@gmail.com>
2025-09-04 17:23:43 +0100

    fixed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://mkuritsu.github.io",
-    //base: "mkuritsu.github.io"
+    trailingSlash: "never",
 });
